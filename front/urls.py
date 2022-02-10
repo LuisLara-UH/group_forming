1b2fa82c10ed0127af363d2128c5f3cb90b5d836
Luis Lara <luis.lara.uh42@gmail.com>
2022-02-09 23:37:41 -0600

    Add student CRUD

--- front/urls.py
+++ front/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home)
+    path('home/', views.home),
+    path('student-list', views.studentList, name='student-list'),
+    path('student-create', views.studentCreate, name='student-create'),
+    path('student-update/<int:id>', views.studentUpdate, name='student-update'),
+    path('student-delete/<int:id>', views.studentDelete, name='student-delete'),
 ]
--- front/views.py
+++ front/views.py
@@ -1,5 +1,46 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Student
+from .forms import StudentForm
 
 def home(request):
     return render(request, 'home.html', {'name': 'World'})
+
+def studentList(request):  
+    students = Student.objects.all()  
+    return render(request,"student-list.html",{'students':students})  
+
+def studentCreate(request):  
+    if request.method == "POST":  
+        form = StudentForm(request.POST)  
+        if form.is_valid():  
+            try:  
+                form.save() 
+                model = form.instance
+                return redirect('/front/student-list')  
+            except:  
+                pass  
+    else:  
+        form = StudentForm()  
+    return render(request,'student-create.html',{'form':form})  
+
+def studentUpdate(request, id):  
+    student = Student.objects.get(id=id)
+    form = StudentForm(initial={'name': student.name, 'last_name': student.last_Name, 'CI': student.identity_Number, 'age': student.age, 'province': student.province})
+    if request.method == "POST":  
+        form = StudentForm(request.POST, instance=student)  
+        if form.is_valid():  
+            try:  
+                form.save() 
+                model = form.instance
+                return redirect('/front/student-list')  
+            except Exception as e: 
+                pass    
+    return render(request,'student-update.html',{'form':form})  
+
+def studentDelete(request, id):
+    student = Student.objects.get(id=id)
+    try:
+        student.delete()
+    except:
+        pass
+    return redirect('/front/student-list')
