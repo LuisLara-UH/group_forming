3244896ce59fc2c357597b558ceefeb72b272c68
Luis Lara <luis.lara.uh42@gmail.com>
2022-02-11 15:30:45 -0600

    Pending fix import_export

--- front/urls.py
+++ front/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home),
+    path('', views.home),
     path('student-list', views.studentList, name='student-list'),
     path('student-create', views.studentCreate, name='student-create'),
     path('student-update/<int:id>', views.studentUpdate, name='student-update'),
--- group_forming/urls.py
+++ group_forming/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('front/', include('front.urls'))
+    path('', include('front.urls'))
 ]
