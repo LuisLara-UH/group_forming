e29638b414031eceb11ff68912752d21d48573cb
Carlos <carlozalejandro98@gmail.com>
2022-02-12 18:02:11 -0300

    update view modified for charging all student data

--- front/views.py
+++ front/views.py
@@ -27,7 +27,44 @@ def studentCreate(request):
 
 def studentUpdate(request, id):  
     student = Student.objects.get(id=id)
-    form = StudentForm(initial={'name': student.name, 'last_name': student.last_Name, 'CI': student.identity_Number, 'age': student.age, 'province': student.province})
+    form = StudentForm(initial={
+        'name': student.name,
+        'last_Name': student.last_Name, 
+        'identity_Number': student.identity_Number, 
+        'age': student.age, 
+        'country': student.country,
+        'municipality': student.municipality,
+        'situation': student.situation,
+        'state': student.state,
+        'address': student.address,
+        'birth_Date': student.birth_Date,
+        'group': student.group,
+        'career': student.career,
+        'faculty': student.faculty,
+        'course_Type': student.course_Type,
+        'mail': student.mail,
+        'source_of_Income': student.source_of_Income,
+        'academic_Origin': student.academic_Origin,
+        'study_Regimen': student.study_Regimen,
+        'natural_From': student.natural_From,
+        'phone_Number': student.phone_Number,
+        'es_Income_Date': student.es_Income_Date,
+        'civil_State': student.civil_State,
+        'political_Organization': student.political_Organization,
+        'ces_Income_Date': student.ces_Income_Date,
+        'enrollment_Date': student.enrollment_Date,
+        'sex': student.sex,
+        'skin_Color': student.skin_Color,
+        'student_Type': student.student_Type,
+        'study_Year': student.study_Year,
+        'work_Center': student.work_Center,
+        'fathers_Name': student.fathers_Name,
+        'fathers_Academic_Level': student.fathers_Academic_Level,
+        'mothers_Name': student.mothers_Name,
+        'mothers_Academic_Level': student.mothers_Academic_Level,
+        'military_Service_Type': student.military_Service_Type,
+        'age': student.age,
+        })
     if request.method == "POST":  
         form = StudentForm(request.POST, instance=student)  
         if form.is_valid():  
