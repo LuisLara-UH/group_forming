e3b8fb8e1017d0f72e63b1d244311b4ac24f79da
Carlos <carlozalejandro98@gmail.com>
2022-02-12 20:12:21 -0300

    working at filters

--- front/static/front/list.js
+++ front/static/front/list.js
@@ -1,10 +1,64 @@
 
+var modelColumnsType = 
+{
+    'Name':'text',
+    'Last Name':'text',
+    'Age':'number',
+    'Country':'text',
+    'Province':'text',
+    'Municipality':'text',
+    'Situation':'text',
+    'State':'text',
+    'Address':'text',
+    'Birth Date':'date',
+    'Group':'text',
+    'Career':'text',
+    'Faculty':'text',
+    'Course Type':'text',
+    'Email':'text',
+    'Source of Income':'text',
+    'Academic Origin':'text',
+    'Study Regimen':'text',
+    'Natural From':'text',
+    'Phone Number':'text',
+    'ES Income Date':'date',
+    'Civil State':'text',
+    'Political Organization':'text',
+    'CES Income Date':'date',
+    'Enrollment Date':'date',
+    'Sex':'text',
+    'Skin Color':'text',
+    'Student Type':'text',
+    'Study Year':'number',
+    'Work Center':'text',
+    'Fathers Name':'text',
+    'Fathers Academic Level':'text',
+    'Mothers Name':'text',
+    'Mothers Academic Level':'text',
+    'Military Service Type':'text'
+}
+
+var columnTypeToHtml=
+{
+    'text':`<input type="text" onChange="changeFilterValue"/>`,
+
+    'date':` <input class="since" type="date" onChange="changeFilterValue"/>
+    -
+    <input class="to" type="date" onChange="changeFilterValue"/>`,
+
+    'number':`<input class="since" type="Number" onChange="changeFilterValue"/>
+    -
+    <input class="to" type="Number" onChange="changeFilterValue"/>`
+}
+
 var trs = document.getElementsByTagName('tr')
 var ths = trs[0].getElementsByTagName("th")
 
 var columnsOn = []
 for(let i = 0; i< ths.length; i++)
     columnsOn.push(ths[i].innerHTML)
+
+   
 
 var columnsOut = []
 
@@ -19,12 +73,13 @@ for(let i = 1; i < trs.length; i++)
     {
         if(tds[j].getElementsByTagName('a').length === 0)
             s[ths[j].innerHTML] = tds[j].innerHTML
+
+        else
+            s.id = tds[j].id
         
     }
     students.push(s)
 }
-
-console.log(students)
 
 function updateColumnsAddRemove()
 {
@@ -98,10 +153,53 @@ function udpateTable()
         for(let i = 0; i < columnsOn.length; i++)
             trBody.innerHTML += ` <td>${students[j][columnsOn[i]]}</td>`
 
+        trBody.innerHTML += `<td style="display:flex"><a type="button" class="btn btn-warning" href="/student-update/${students[j].id}">Editar</a>
+        <a type="button" class="btn btn-danger" href="/student-delete/${students[j].id}">Eliminar</a>
+    </td>`
         tBody.appendChild(trBody)
         
     }
-
 }
+
+function changeFilterName(select)
+{
+    let filterItem = select.parentNode
+    let valueContainer = filterItem.getElementsByClassName('valueContainer')[0]
+    valueContainer.innerHTML=''
+    valueContainer.innerHTML = columnTypeToHtml[modelColumnsType[select.value]]
+}
+
+function addFilter()
+{
+    let filtersContainer = document.getElementById('filtersContainer');
+    let filterItem = document.createElement('div')
+    filterItem.className = "filterItem"
+    let selectFilter = document.createElement('select');
+    selectFilter.className="filterSelect"
+  
+    let filtersName = Object.keys(modelColumnsType);
+    for(let i = 0; i < filtersName.length; i++)
+    {
+        selectFilter.innerHTML += `<option id=${filtersName[i]}>${filtersName[i]}</option>`
+    }
+    
+    filterItem.appendChild(selectFilter)
+    filterItem.innerHTML += ` <div class="valueContainer" style="display:flex"> 
+    <input type="text" onChange="changeFilterValue"/>
+    </div>
+    <div class="buttonRemove" onClick='removeFilter(this)'>-</div>`
+filterItem.addEventListener
+    filtersContainer.appendChild(filterItem)
+    let selects = filtersContainer.getElementsByTagName('select');
+    selects[selects.length - 1].addEventListener('change', changeFilterName.bind(this, selects[selects.length - 1]))
+}
+
+function removeFilter(element)
+{
+    let filtersContainer = document.getElementById('filtersContainer');
+    filtersContainer.removeChild(element.parentNode)
+}
+
+
 
 updateColumnsAddRemove()
