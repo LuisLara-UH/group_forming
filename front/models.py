4840da68c2f1cb9bd9dc1a46b4cf536783aeb77f
Luis Alejandro Lara Rojas <luisalejandrolararojas@Luiss-MacBook-Pro.local>
2022-02-13 13:49:03 -0600

    Minor updates

--- front/models.py
+++ front/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+import front.models
 from .model_choices import *
 
 
@@ -8,19 +10,23 @@ class Student(models.Model):
     last_Name = models.CharField(db_column='last_name', max_length=100, blank=False)
     country = models.CharField(max_length=100, choices=CountryChoices.choices, default=CountryChoices.Cuba)
     province = models.CharField(max_length=100, choices=ProvinceChoices.choices, default=ProvinceChoices.Habana)
-    municipality = models.CharField(max_length=100, choices=MunicipalityChoices.choices, default=MunicipalityChoices.Playa)
+    municipality = models.CharField(max_length=100, choices=MunicipalityChoices.choices,
+                                    default=MunicipalityChoices.Playa)
     situation = models.CharField(max_length=100, choices=SituationChoices.choices, default=StateChoices.Active)
     state = models.CharField(max_length=100, choices=StateChoices.choices, default=SituationChoices.New_Income)
     address = models.CharField(db_column='address', max_length=100, blank=False)
     birth_Date = models.DateField(db_column='birth_date', max_length=100, blank=False)
-    group = models.CharField(db_column='group', max_length=100, blank=False)
     career = models.CharField(max_length=100, choices=CareerChoices.choices, default=CareerChoices.Computer_Science)
     faculty = models.CharField(max_length=100, choices=FacultyChoices.choices, default=FacultyChoices.Matcom)
-    course_Type = models.CharField(max_length=100, choices=CourseTypeChoices.choices, default=CourseTypeChoices.Daily)
+    course_Type = models.CharField(max_length=100, choices=CourseTypeChoices.choices,
+                                   default=CourseTypeChoices.Daily)
     mail = models.EmailField(db_column='mail', max_length=100, blank=False)
-    source_of_Income = models.CharField(max_length=100, choices=SourceOfIncomeChoices.choices, default=SourceOfIncomeChoices.PreUniversitary)
-    academic_Origin = models.CharField(max_length=100, choices=AcademicOriginChoices.choices, default=AcademicOriginChoices.PreUniversitary)
-    study_Regimen = models.CharField(max_length=100, choices=StudyRegimenChoices.choices, default=StudyRegimenChoices.Only_Study)
+    source_of_Income = models.CharField(max_length=100, choices=SourceOfIncomeChoices.choices,
+                                        default=SourceOfIncomeChoices.PreUniversitary)
+    academic_Origin = models.CharField(max_length=100, choices=AcademicOriginChoices.choices,
+                                       default=AcademicOriginChoices.PreUniversitary)
+    study_Regimen = models.CharField(max_length=100, choices=StudyRegimenChoices.choices,
+                                     default=StudyRegimenChoices.Only_Study)
     natural_From = models.CharField(db_column='natural_from', max_length=100, blank=False)
     phone_Number = models.CharField(db_column='phone_number', max_length=100, blank=False)
     es_Income_Date = models.DateField(db_column='es_income_date', max_length=100, blank=False)
@@ -30,16 +36,25 @@ class Student(models.Model):
     enrollment_Date = models.DateField(db_column='enrollment_date', max_length=100, blank=False)
     sex = models.CharField(db_column='sex', max_length=100, blank=False)
     skin_Color = models.CharField(max_length=100, choices=SkinColorChoices.choices, default=SkinColorChoices.White)
-    student_Type = models.CharField(max_length=100, choices=StudentTypeChoices.choices, default=StudentTypeChoices.Extern)
+    student_Type = models.CharField(max_length=100, choices=StudentTypeChoices.choices,
+                                    default=StudentTypeChoices.Extern)
     study_Year = models.CharField(db_column='study_year', max_length=100, blank=False)
     work_Center = models.CharField(db_column='work_center', max_length=100, blank=False)
     fathers_Name = models.CharField(db_column='fathers_name', max_length=100, blank=False)
-    fathers_Academic_Level = models.CharField(max_length=100, choices=AcademicLevelChoices.choices, default=AcademicLevelChoices.Superior)
+    fathers_Academic_Level = models.CharField(max_length=100, choices=AcademicLevelChoices.choices,
+                                              default=AcademicLevelChoices.Superior)
     mothers_Name = models.CharField(db_column='mothers_name', max_length=100, blank=False)
-    mothers_Academic_Level = models.CharField(max_length=100, choices=AcademicLevelChoices.choices, default=AcademicLevelChoices.Superior)
+    mothers_Academic_Level = models.CharField(max_length=100, choices=AcademicLevelChoices.choices,
+                                              default=AcademicLevelChoices.Superior)
     military_Service_Type = models.CharField(db_column='military_service_type', max_length=100)
     age = models.PositiveIntegerField(db_column='age', blank=False)
 
+    # Relational fields
+    # group = models.ForeignKey(front.GroupModel, db_column='group')
+
+
 class GroupModel(models.Model):
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length=50)
+
+    # relational fields
     students = models.ManyToManyField(Student, db_column='group_students')
--- front/views.py
+++ front/views.py
@@ -4,43 +4,47 @@ from .forms import *
 from .utils import import_excel_file
 from .services import *
 
+
 def home(request):
     # TODO: Fill this view with the initial page
     return render(request, 'home.html', {'name': 'World'})
 
 
+"""Student"""
 def student_list(request):
-    students = Student.objects.all()  
+    students = Student.objects.all()
     return render(request, "student-list.html", {'students': students})
 
 
 def student_create(request):
-    if request.method == "POST":  
-        form = StudentForm(request.POST)  
-        if form.is_valid():  
-            try:  
-                form.save() 
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
                 model = form.instance
-                return redirect('student-list')  
-            except:  
-                pass  
-    else:  
-        form = StudentForm()  
+                return redirect('student-list')
+            except Exception as e:
+                print("There was an exception creating the student: " + e)
+    else:
+        form = StudentForm()
+
     return render(request, 'student-create.html', {'form': form})
 
 
 def student_update(request, id):
     student = Student.objects.get(id=id)
     form = get_student_form(student)
-    if request.method == "POST":  
-        form = StudentForm(request.POST, instance=student)  
-        if form.is_valid():  
-            try:  
-                form.save() 
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            try:
+                form.save()
                 model = form.instance
-                return redirect('student-list')  
-            except Exception as e: 
-                pass    
+                return redirect('student-list')
+            except Exception as e:
+                print("There was an exception updating the student: " + e)
+
     return render(request, 'student-update.html', {'form': form})
 
 
@@ -48,36 +52,41 @@ def student_delete(request, id):
     student = Student.objects.get(id=id)
     try:
         student.delete()
-    except:
-        pass
+    except Exception as e:
+        print("There was an exception deleting the student: " + e)
+
     return redirect('student-list')
 
 
+"""Upload Files"""
 def upload_file(request):
     excel_file = request.FILES["excel_file"]
     import_excel_file(excel_file)
 
     return redirect('student-list')
 
-##########################Groups###############################################
+
+"""Groups"""
 def group_list(request):
     groups = GroupModel.objects.all()
-    groupsView = []
+    groups_view = []
     for g in groups:
-        groupsView.append({'group':g, 'countStudents':len(g.students.all())})
-    
-    return render(request, 'group-list.html', {'groups':groupsView})
+        groups_view.append({'group': g, 'countStudents': len(g.students.all())})
+
+    return render(request, 'group-list.html', {'groups': groups_view})
+
 
 def change_group_name(request, id):
     group = GroupModel.objects.get(id=id)
-    form = {'name':group.name}
-    if request.method == "POST":  
+    form = {'name': group.name}
+    if request.method == "POST":
         new_name = request.POST['name']
-        if new_name != '':  
-            try:  
+        if new_name != '':
+            try:
                 group.name = new_name
                 group.save()
-                return redirect('group-list')  
-            except Exception as e: 
-                print("que ha pasao")  
+                return redirect('group-list')
+            except Exception as e:
+                print("There was an exception updating the group: " + e)
+
     return render(request, 'change-group-name.html', {'form': form})
